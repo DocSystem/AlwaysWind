a71ffa59f6d85a02ed23d20dec26a21821af59ec
DocSystem <42613513+DocSystem@users.noreply.github.com>
2022-07-31 14:30:10 +0200

    Initial Commit

---
+++ AlwaysWind/AlwaysWindApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlwaysWindApp.swift
+//  AlwaysWind
+//
+//  Created by Antoine Souben-Fink on 31/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct AlwaysWindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlwaysWind/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  AlwaysWind
+//
+//  Created by Antoine Souben-Fink on 31/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
